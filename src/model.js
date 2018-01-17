702ead8f9d218dc4be5171127091b4cc28f87f8b
Andrei Dobrinski <>
2018-01-16 19:47:16 -0500

    remove existing tests

--- src/model.js
+++ src/model.js
@@ -1,6 +1,5 @@
 import * as MS from 'microstates'
 
 export default class LoginModel {
-  username = MS.String
-  password = MS.String
+  
 }
--- stories/model.test.js
+++ stories/model.test.js
@@ -4,13 +4,4 @@ import expect from 'expect'
 import Model from '../src/model'
 import microstate from 'microstates'
 
-export default () =>
-  describe('Model', function() {
-    let ms = microstate(Model)
-    it('has empty username field', function() {
-      expect(ms.state.username).toBeTruthy()
     })
-    it('has empty password field', function() {
-      expect(ms.state.password).toBeTruthy()
-    })
-  })
