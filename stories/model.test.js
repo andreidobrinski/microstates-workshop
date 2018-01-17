0a96105a9d6f0e23814c857ff92927fc654dcb4d
Andrei Dobrinski <>
2018-01-16 19:47:46 -0500

    add test to expect email field

--- stories/model.test.js
+++ stories/model.test.js
@@ -4,4 +4,8 @@ import expect from 'expect'
 import Model from '../src/model'
 import microstate from 'microstates'
 
+export default () => describe('Model', function() {
+    it('has email address field', function() {
+      expect(ms.state.email).toExist()
     })
+})
